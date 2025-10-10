60168eec91ff4d549b4e50b6990fdb6c5e4c2a12
Saurabh <saurabhacharya05@gmail.com>
2025-10-10 20:45:04 +0530

    add loan.json with initial loan data

--- DhanSaathi/frontend/src/App.tsx
+++ DhanSaathi/frontend/src/App.tsx
@@ -1,8 +1,32 @@
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import AuthModals from "./pages/AuthModals";
+import Dashboard from "./pages/Dashboard";
+import LoanEligibility from "./pages/LoanEligibility";
+import SchemeChecker from "./pages/SchemeChecker";
+import FormHelper from "./pages/FormHelper";
 import HomePage from "./pages/HomePage";
+import { Toaster } from "sonner";
+
 
 function App() {
-  return <HomePage />;
+  return (
+    <>
+      <Toaster position="top-center" />
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/auth" element={<AuthModals />} />
+          <Route path="/dashboard" element={<Dashboard />} />
+          <Route path="/eligibility" element={<LoanEligibility />} />
+          <Route path="/scheme-checker" element={<SchemeChecker />} />
+          <Route path="/form-helper" element={<FormHelper />} />
+        
+
+
+        </Routes>
+      </Router>
+    </>
+  );
 }
 
 export default App;
